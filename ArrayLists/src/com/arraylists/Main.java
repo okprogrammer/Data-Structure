43da4e1abab770f16c0fdfb286f19607147e52ac
okprogrammmer <omk7912@gmail.com>
2018-04-19 00:53:15 +0530

    implementation of arraylists

---
+++ ArrayLists/src/com/arraylists/Main.java
@@ -0,0 +1,39 @@
+package com.arraylists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Jane","Jones",123));
+        employeeList.add(new Employee("Jone","Doe",54567));
+        employeeList.add(new Employee("Mary","Smith",22));
+        employeeList.add(new Employee("Mike","Wilson",345));
+
+       // employeeList.forEach(employee->System.out.println(employee));
+
+        //System.out.println(employeeList.get(1));
+       // System.out.println(employeeList.isEmpty());
+        employeeList.set(1,new Employee("John","Adams",4568));
+
+        //System.out.println(employeeList.size());
+        employeeList.add(3,new Employee("John","Doe",4567));
+       // employeeList.forEach(employee->System.out.println(employee));
+
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+
+        //for (Employee employee:employeeArray)
+        //{
+          //  System.out.println(employee);
+        //}
+
+        //System.out.println(employeeList.contains(new Employee("Mary","Smith",22)));
+        //System.out.println(employeeList.indexOf(new Employee("Mary","Smith",22)));
+
+        employeeList.remove(2);
+        employeeList.forEach(employee->System.out.println(employee));
+
+    }
+}
